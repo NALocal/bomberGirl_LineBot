8723214091090da2ae37547e5f5932ec2f26d24c
TW-IT-Lion Chuang <lion.chuang@shoalter.com>
2022-10-26 15:23:34 +0800

    init

--- jsHome/CatchFunction.js
+++
@@ -1,9 +0,0 @@
-exports.ErrorDo = function(data, name = "Error") {
-    console.log(`${name} ${data}`);
-}
-
-exports.LogDo = function(data, name = "") {
-    console.log(`${name} ${data}`);
-}
-
-exports.EmptyDo = function(data = "") {}
